ba2d578e1eac2406d1e997e938e51707447d79f3
UnityGitApps <UnityGitApps@users.noreply.github.com>
2024-05-14 12:01:58 +0000

    Deploying to webpage from @ Unity-2022/com.row.match3riger@d0208d4af476b19873cb38266e5c5f6fe3cdfdfe 🚀

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Fortune Tuger-0.0.3";
+const cacheName = "DefaultCompany-Fortune Tuger-0.0.4";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.br",
